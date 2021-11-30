ac5e2931955df29d6192f73bd867ce8d311579a5
Alex Carrera <a_fcalvarez@hotmail.com>
2021-11-30 17:06:42 -0500

    header, slider

--- src/app/core/shared/shared.module.ts
+++ src/app/core/shared/shared.module.ts
@@ -2,22 +2,28 @@ import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http'
 import { RouterModule } from '@angular/router';
 
+import {MatIconModule} from '@angular/material/icon'; 
 
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
 
   imports: [
     HttpClientModule,
-    RouterModule
+    RouterModule,
+    MatIconModule
   ],
   declarations: [
-    NotFoundComponent
+    NotFoundComponent,
+    HeaderComponent
   ],
   exports: [
     HttpClientModule,
     RouterModule,
-    NotFoundComponent
+    NotFoundComponent,
+    HeaderComponent,
+    MatIconModule
   ],
   providers:[]
 })
--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { PublicRoutingModule } from './public-routing.module';
 
+
 import { SharedModule } from '../core/shared/shared.module';
 import { PublicComponent } from './public.component';
 import { HomeComponent } from './home/containers/home.component';
 import { LoginComponent } from './login/login.component';
+
 
 
 @NgModule({
