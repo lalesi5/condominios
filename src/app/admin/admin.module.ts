551b2ab55e3da357e35704d998a7bf174434c0e0
Alex Carrera <a_fcalvarez@hotmail.com>
2021-12-03 23:49:33 -0500

    adding admin to app.module

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http'
 import { RouterModule } from '@angular/router';
 
-import { SharedModule } from '../../core/shared/shared.module';
-import { NavigationComponent } from './components/navigation/navigation.component';
+import { SharedModule } from '../core/shared/shared.module';
 
 
 @NgModule({
@@ -14,12 +13,10 @@ import { NavigationComponent } from './components/navigation/navigation.componen
         SharedModule
     ],
     declarations: [
-        NavigationComponent
     ],
     exports: [
         HttpClientModule,
         RouterModule,
-        NavigationComponent
     ],
     providers: []
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AdminModule } from './admin/admin.module';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     BrowserModule,
     AppRoutingModule,
     CoreModule,
+    AdminModule,
     BrowserAnimationsModule,
     NgbModule
   ],
