ed342441275a93c66d05e7cfaade19c7a83f9489
Alex Carrera <a_fcalvarez@hotmail.com>
2021-12-03 23:47:22 -0500

    creacion componente admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
 
 import { NotFoundComponent } from './core/shared/components/not-found/not-found.component';
 
 const routes: Routes = [
   {path: '', loadChildren: () => import('./public/public.module').then(m => m.PublicModule)},
+  {path: 'admin', component: AdminComponent},
   {path: '**', component: NotFoundComponent}
 ];
 
