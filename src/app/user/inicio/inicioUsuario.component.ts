c5337fbd1b348be9f88532331602e06a5dcc1ff3
Alex Carrera <a_fcalvarez@hotmail.com>
2022-03-19 22:13:15 -0500

    anuncios generales modulo usuarios

--- src/app/user/inicio/inicioUsuario.component.ts
+++ src/app/user/inicio/inicioUsuario.component.ts
@@ -1,5 +1,7 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, OnDestroy, OnInit} from "@angular/core";
 import {Router} from "@angular/router";
+import {Subscription} from "rxjs";
+import {AnunciosGeneralesService} from "../../services/anunciosGenerales.service";
 
 @Component({
   selector: 'app-inicioUsuario',
@@ -7,20 +9,44 @@ import {Router} from "@angular/router";
   styleUrls: ['./inicioUsuario.component.css']
 })
 
-export class InicioUsuarioComponent implements OnInit {
+export class InicioUsuarioComponent implements OnInit, OnDestroy {
+
+  private subscription: Subscription = new Subscription;
+  idCondominio: string ='';
+  anunciosGenerales: any[] = [];
   unidad: any[] = [];
 
   constructor(
-    private router: Router
+    private router: Router,
+    private _anunciosGeneralesService: AnunciosGeneralesService,
   ) {
     this.recoverData()
   }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.getAnunciosGenerales();
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
   recoverData() {
     const navigations: any = this.router.getCurrentNavigation()?.extras.state;
+    this.idCondominio = navigations.idCondominio;
     this.unidad = navigations;
+  }
+
+  getAnunciosGenerales() {
+    this.subscription.add(
+      this._anunciosGeneralesService.getAnunciosGenerales(this.idCondominio).subscribe(data => {
+        this.anunciosGenerales = [];
+        data.forEach((element: any) => {
+          this.anunciosGenerales.push({
+            ...element.payload.doc.data()
+          })
+        })
+      })
+    );
   }
 }
