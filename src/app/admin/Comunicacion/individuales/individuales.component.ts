644dc33706a97d33c3cafad8844c04b64eb1fd09
Alejandro Tupiza <ad_tp@hotmail.com>
2022-04-03 02:19:48 -0500

    tabla comunicados individuales

--- src/app/admin/Comunicacion/individuales/individuales.component.ts
+++ src/app/admin/Comunicacion/individuales/individuales.component.ts
@@ -1,7 +1,9 @@
-import {Component, OnInit} from "@angular/core";
-import {Router} from "@angular/router";
-import {UnidadesService} from '../../../services/unidades.service';
-import {Subscription} from "rxjs";
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { Router } from "@angular/router";
+import { UnidadesService } from '../../../services/unidades.service';
+import { Subscription } from "rxjs";
+import { Query } from '@syncfusion/ej2-data';
+import { CommandModel, GridComponent, PageSettingsModel, PdfExportProperties, ToolbarItems } from "@syncfusion/ej2-angular-grids";
 
 @Component({
   selector: 'app-individuales',
@@ -16,10 +18,20 @@ export class IndividualesComponent implements OnInit {
   idCondominio: string = '';
   unidades: any[] = [];
 
+  public pageSettings: PageSettingsModel;
+  public toolbarOptions: ToolbarItems[];
+  public commands: CommandModel[];
+  public queryClone: any;
+  @ViewChild('grid') public grid: GridComponent | any;
+
   constructor(
     private router: Router,
     private _unidadesService: UnidadesService
   ) {
+    this.pageSettings = { pageSize: 6 }
+    this.toolbarOptions = ['PdfExport', 'ExcelExport', 'Search'];
+    this.commands = [{ title: 'Ver Mensaje', buttonOption: { iconCss: 'e-icons e-comments', cssClass: 'e-flat' } },
+    { title: 'Nuevo', buttonOption: { iconCss: 'e-icons e-save-as', cssClass: 'e-flat' } }];
     this.recoverData();
   }
 
@@ -30,7 +42,6 @@ export class IndividualesComponent implements OnInit {
   recoverData() {
     this.idCondominio = <string>sessionStorage.getItem('idCondominio');
   }
-
 
   getUnidades() {
     this.subscription.add(
@@ -47,6 +58,20 @@ export class IndividualesComponent implements OnInit {
     )
   }
 
+  //Seleccionar editar o eliminar usuario
+  commandClick(item: any): void {
+    if (item.target?.title === 'Ver Mensaje') {
+      sessionStorage.setItem('idUnidad', <string>item.rowData['idUnidad']);
+      sessionStorage.setItem('idUsuario', <string>item.rowData['idUsuario']);
+      sessionStorage.setItem('nombreResidente', <string>item.rowData['nombreResidente']);
+      sessionStorage.setItem('apellidoResidente', <string>item.rowData['apellidoResidente']);
+      this.router.navigate(['/admin/comunicacion/mensajeUsuario']);
+    } else if (item.target?.title === 'Nuevo') {
+      sessionStorage.setItem('idUnidad', <string>item.rowData['idUnidad']);
+      this.router.navigate(['/admin/comunicacion/nuevoMensaje']);
+    }
+  }
+
   onGoMensajesUsuarios(item: any) {
     sessionStorage.setItem('idUnidad', <string>item.idUnidad);
     sessionStorage.setItem('idUsuario', <string>item.idUsuario);
@@ -58,5 +83,42 @@ export class IndividualesComponent implements OnInit {
   onGoNuevoMensaje(item: any) {
     sessionStorage.setItem('idUnidad', <string>item.idUnidad);
     this.router.navigate(['/admin/comunicacion/nuevoMensaje']);
+  }
+
+  //Seleccionar exportar excel y pdf
+  toolbarClick(args: any): void {
+    if (args.item.id === 'Grid_pdfexport') {
+      const pdfExportProperties: PdfExportProperties = {
+        fileName: 'usuarios.pdf'
+      };
+      this.queryClone = this.grid.query;
+      this.grid.query = new Query().addParams('recordcount', '12');
+      this.grid.pdfExport(pdfExportProperties);
+      //this.grid.pdfExport();
+    } else if (args.item.id === 'Grid_excelexport') {
+      this.queryClone = this.grid.query;
+      this.grid.query = new Query().addParams('recordcount', '12');
+      this.grid.excelExport();
+    }
+  }
+
+  pdfExportComplete(): void {
+    this.grid.query = this.queryClone;
+  }
+
+  excelExportComplete(): void {
+    this.grid.query = this.queryClone;
+  }
+
+  rowDataBound(args: any) {
+    // aquí estamos calculando el número de serie
+    var rowIndex = parseInt(args.row.getAttribute('aria-rowIndex'));
+    var page = this.grid.pageSettings.currentPage! - 1;
+
+    var totalPages = this.grid.pageSettings.pageSize;
+    var startIndex = page * totalPages!;
+    var sno = startIndex + (rowIndex + 1);
+    //  actualizando el valor en la primera celda de la fila donde hemos representado una columna vacía para esto
+    args.row.cells[0].innerText = sno;
   }
 }
