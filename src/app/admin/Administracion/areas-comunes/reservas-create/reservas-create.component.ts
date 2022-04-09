4e6ae50244b33d2e176d5c6f388a271b23d84e33
Alejandro Tupiza <ad_tp@hotmail.com>
2022-04-09 01:22:55 -0500

    Crear reservas y listar

--- src/app/admin/Administracion/areas-comunes/reservas-create/reservas-create.component.ts
+++ src/app/admin/Administracion/areas-comunes/reservas-create/reservas-create.component.ts
@@ -1,15 +1,156 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Subscription } from 'rxjs';
+import { DialogService } from 'src/app/services/dialog.service';
+import { ReservasService } from 'src/app/services/reservas.service';
+import { UnidadesService } from 'src/app/services/unidades.service';
 
 @Component({
   selector: 'app-reservas-create',
   templateUrl: './reservas-create.component.html',
   styleUrls: ['./reservas-create.component.css']
 })
-export class ReservasCreateComponent implements OnInit {
+export class ReservasCreateComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private subscription: Subscription = new Subscription;
+  idAdministrador: string = '';
+  idCondominio: string = '';
+  idAreaComunal: string = '';
+  nombreAreaComunal: string = '';
+  numeroUnidad: string = '';
+  loading = false;
+  areaComunal: any[] = [];
+  unidades: any[] = [];
+  unidad: any[] = [];
+
+  reservaForm: FormGroup;
+  datosUnidadForm: FormGroup;
+
+  constructor(
+    private router: Router,
+    private _unidadesService: UnidadesService,
+    private _reservaService: ReservasService,
+    private fb: FormBuilder,
+    private _dialogService: DialogService,
+    private toastr: ToastrService
+  ) {
+    this.reservaForm = this.fb.group({
+      fechaReservaInicio: ['', Validators.required],
+      fechaReservaFin: ['', Validators.required],
+      detalleReserva: ['', Validators.required],
+      estadoReserva: ['', Validators.required],
+      idUnidad: ['', Validators.required],
+    });
+
+    this.datosUnidadForm = this.fb.group({
+      nombreResidente: [''],
+      apellidoResidente: [''],
+      unidad: ['']
+    })
+    this.recoverData();
+  }
 
   ngOnInit(): void {
+    this.getDatosUnidades();
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  recoverData() {
+    this.idAdministrador = <string>sessionStorage.getItem('idAdministrador');
+    this.idCondominio = <string>sessionStorage.getItem('idCondominio');
+    this.idAreaComunal = <string>sessionStorage.getItem('idAreaComunal');
+    this.nombreAreaComunal = <string>sessionStorage.getItem('nombreAreaComunal');
+  }
+
+  getDatosUnidades() {
+    this.subscription.add(
+      this._unidadesService.getAllUnidades(this.idCondominio).subscribe(data => {
+        this.unidades = [];
+        data.forEach((element: any) => {
+          this.unidades.push({
+            ...element.payload.doc.data()
+          })
+        })
+      })
+    );
+  }
+
+  onUnitChanged(item: any) {
+    this.subscription.add(
+      this._unidadesService.getUnidad(item).subscribe(data => {
+        this.loading = false;
+        this.datosUnidadForm.setValue({
+          nombreResidente: data.payload.data()['nombreResidente'],
+          apellidoResidente: data.payload.data()['apellidoResidente'],
+          unidad: data.payload.data()['unidad']
+        })
+      })
+    )
+  }
+
+  onCreateReserva() {
+    const descripcionArea = String(this.reservaForm.value.detalleReserva).charAt(0).toLocaleUpperCase() + String(this.reservaForm.value.detalleReserva).slice(1);
+    const date = this.reservaForm.value.fechaReservaInicio;
+    const date2 = this.reservaForm.value.fechaReservaFin;
+    const idUnidad = this.reservaForm.value.idUnidad;
+
+    this._dialogService.confirmDialog({
+      title: 'Agregar reserva',
+      message: '¿Está seguro de agregar la reserva?',
+      confirmText: 'Si',
+      cancelText: 'No',
+    }).subscribe(res => {
+      if (res) {
+
+        const reserva: any = {
+          fechaReservaInicio: date.toLocaleString(),
+          fechaReservaFin: date2.toLocaleString(),
+          estadoReserva: this.reservaForm.value.estadoReserva,
+          idUnidad: idUnidad,
+          unidad: this.datosUnidadForm.value.unidad,
+          nombreResidente: this.datosUnidadForm.value.nombreResidente,
+          apellidoResidente: this.datosUnidadForm.value.apellidoResidente,
+          idAdministrador: this.idAdministrador,
+          idCondominio: this.idCondominio,
+          idAreaComunal: this.idAreaComunal,
+          nombreAreaComunal: this.nombreAreaComunal,
+          detalleReserva: descripcionArea
+        }
+
+        this.loading = true;
+        this._reservaService.agregarReserva(reserva).then(() => {
+          this.toastr.success('La reserva fue creada con exito', 'Reserva registrada', {
+            positionClass: 'toast-bottom-rigth'
+          });
+          this.loading = false;
+          this.router.navigate(['/admin/administracion/areasComunes/reservasPendientes']);
+        }).catch(error => {
+          console.log(error);
+        })
+      }
+    })
+  }
+
+  getUnidadID(idUser: string) {
+    this.subscription.add(
+      this._unidadesService.getUnidadesById(idUser).subscribe(data => {
+        this.unidad = [];
+        data.forEach((element: any) => {
+          this.unidad.push({
+            ...element.payload.doc.data()
+          })
+        })
+      })
+    );
+  }
+
+  onBacktoList(): void {
+    this.router.navigate(['admin/administracion/areasComunes/reservasPendientes']);
   }
 
 }
--- src/app/admin/Administracion/areas-comunes/reservas-pendientes/reservas-pendientes.component.ts
+++ src/app/admin/Administracion/areas-comunes/reservas-pendientes/reservas-pendientes.component.ts
@@ -1,15 +1,150 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommandModel, GridComponent, PageSettingsModel, PdfExportProperties, ToolbarItems } from '@syncfusion/ej2-angular-grids';
+import { Query } from '@syncfusion/ej2-data';
+import { ToastrService } from 'ngx-toastr';
+import { Subscription } from 'rxjs';
+import { DialogService } from 'src/app/services/dialog.service';
+import { ReservasService } from 'src/app/services/reservas.service';
 
 @Component({
   selector: 'app-reservas-pendientes',
   templateUrl: './reservas-pendientes.component.html',
   styleUrls: ['./reservas-pendientes.component.css']
 })
-export class ReservasPendientesComponent implements OnInit {
+export class ReservasPendientesComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private subscription: Subscription = new Subscription;
+  idAreaComunal: string = '';
+  nombreAreaComunal: string = '';
+  reservas: any[] = [];
 
-  ngOnInit(): void {
+  public pageSettings: PageSettingsModel;
+  public toolbarOptions: ToolbarItems[];
+  public commands: CommandModel[];
+  public queryClone: any;
+  @ViewChild('grid') public grid: GridComponent | any;
+
+  constructor(
+    private router: Router,
+    private _reservaService: ReservasService,
+    private toastr: ToastrService,
+    private _dialogService: DialogService
+  ) {
+    this.pageSettings = { pageSize: 6 }
+    this.toolbarOptions = ['PdfExport', 'ExcelExport', 'Search'];
+    this.commands = [
+      { title: 'Aceptar', buttonOption: { iconCss: 'e-icons e-circle-check', cssClass: 'e-flat' } },
+      { title: 'Rechazar', buttonOption: { iconCss: 'e-icons e-circle-close', cssClass: 'e-flat' } }
+    ];
+
+    this.recoverData();
   }
 
+  ngOnInit(): void {
+    this.getReservas();
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  recoverData() {
+    this.idAreaComunal = <string>sessionStorage.getItem('idAreaComunal');
+    this.nombreAreaComunal = <string>sessionStorage.getItem('nombreAreaComunal');
+  }
+
+  getReservas() {
+    this.subscription.add(
+      this._reservaService.getReserva(this.idAreaComunal).subscribe(data => {
+        this.reservas = [];
+        data.forEach((element: any) => {
+          this.reservas.push({
+            ...element.payload.doc.data()
+          })
+        })
+      })
+    );
+  }
+
+  onGoCreate() {
+    this.router.navigate(['/admin/administracion/areasComunes/reservasCreate']);
+  }
+
+  //Seleccionar editar o eliminar usuario
+  commandClick(item: any): void {
+    const id = <string>item.rowData['idReserva'];
+    if (item.target?.title === 'Aceptar') {
+      this._dialogService.confirmDialog({
+        title: 'Aceptar Reserva',
+        message: '¿Está seguro de aceptar la reserva?',
+        confirmText: 'Si',
+        cancelText: 'No',
+      }).subscribe(res => {
+        if (res) {
+
+          const reserva: any = {
+            estadoReserva: 'Aceptado',
+          }
+
+          this._reservaService.actualizarReserva(id, reserva).then(() => {
+            this.toastr.success('La reserva fue actualizada con exito', 'Reserva actualizada', {
+              positionClass: 'toast-bottom-rigth'
+            });
+          }).catch(error => {
+            console.log(error);
+          })
+        }
+      })
+
+    } else if (item.target?.title === 'Rechazar') {
+
+      this._dialogService.confirmDialog({
+        title: 'Aceptar Reserva',
+        message: '¿Está seguro de rechazar la reserva?',
+        confirmText: 'Si',
+        cancelText: 'No',
+      }).subscribe(res => {
+        if (res) {
+
+          const reserva: any = {
+            estadoReserva: 'Rechazado',
+          }
+
+          this._reservaService.actualizarReserva(id, reserva).then(() => {
+            this.toastr.success('La reserva fue actualizada con exito', 'Reserva actualizada', {
+              positionClass: 'toast-bottom-rigth'
+            });
+          }).catch(error => {
+            console.log(error);
+          })
+        }
+      })
+    }
+  }
+
+  //Seleccionar exportar excel y pdf
+  toolbarClick(args: any): void {
+    if (args.item.id === 'Grid_pdfexport') {
+      const pdfExportProperties: PdfExportProperties = {
+        fileName: 'usuarios.pdf'
+      };
+      this.queryClone = this.grid.query;
+      this.grid.query = new Query().addParams('recordcount', '12');
+      this.grid.pdfExport(pdfExportProperties);
+      //this.grid.pdfExport();
+    } else if (args.item.id === 'Grid_excelexport') {
+      this.queryClone = this.grid.query;
+      this.grid.query = new Query().addParams('recordcount', '12');
+      this.grid.excelExport();
+    }
+  }
+
+  pdfExportComplete(): void {
+    this.grid.query = this.queryClone;
+  }
+
+  excelExportComplete(): void {
+    this.grid.query = this.queryClone;
+  }
 }
