b5a886415910bf90666db0399c433d2b9dafca4d
Alex Carrera <a_fcalvarez@hotmail.com>
2022-05-09 23:58:05 -0500

    ingresos unidades y extraordinarios reportes

--- src/app/admin/Finanzas/finanzas.module.ts
+++ src/app/admin/Finanzas/finanzas.module.ts
@@ -1,21 +1,25 @@
-import { NgModule } from '@angular/core';
-import { FinanzasRoutingModule } from './finanzas-routing.module';
+import {NgModule} from '@angular/core';
+import {FinanzasRoutingModule} from './finanzas-routing.module';
 
 import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
 import {MatSelectModule} from '@angular/material/select';
 
-import { EgresosFinanzasComponent } from './egresosFinanzas/egresosFinanzas.component';
-import { IngresosExtraordinariosFinanzasComponent } from './ingresosExtraordinariosFinanzas/ingresosExtraordinariosFinanzas.component';
-import { IngresosFinanzasComponent } from './ingresosFinanzas/ingresosFinanzas.component';
+import {EgresosFinanzasComponent} from './egresosFinanzas/egresosFinanzas.component';
+import {
+  IngresosExtraordinariosFinanzasComponent
+} from './ingresosExtraordinariosFinanzas/ingresosExtraordinariosFinanzas.component';
+import {IngresosFinanzasComponent} from './ingresosFinanzas/ingresosFinanzas.component';
 import {GridModule} from "@syncfusion/ej2-angular-grids";
-import { RegistroMensualidadComponent } from './registro-mensualidad/registro-mensualidad.component';
+import {RegistroMensualidadComponent} from './registro-mensualidad/registro-mensualidad.component';
 import {CommonModule} from "@angular/common";
-import { CrearPagoMensualidadComponent } from './crear-pago-mensualidad/crear-pago-mensualidad.component';
+import {CrearPagoMensualidadComponent} from './crear-pago-mensualidad/crear-pago-mensualidad.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import { RegistrarExtraordinariosFinanzasComponent } from './registrar-extraordinarios-finanzas/registrar-extraordinarios-finanzas.component';
-import { RegistrarEgresoComponent } from './registrar-egreso/registrar-egreso.component';
+import {
+  RegistrarExtraordinariosFinanzasComponent
+} from './registrar-extraordinarios-finanzas/registrar-extraordinarios-finanzas.component';
+import {RegistrarEgresoComponent} from './registrar-egreso/registrar-egreso.component';
 
 @NgModule({
 
@@ -30,26 +34,27 @@ import { RegistrarEgresoComponent } from './registrar-egreso/registrar-egreso.co
     FormsModule,
     ReactiveFormsModule
   ],
-    declarations: [
-        EgresosFinanzasComponent,
-        IngresosExtraordinariosFinanzasComponent,
-        IngresosFinanzasComponent,
-        RegistroMensualidadComponent,
-        CrearPagoMensualidadComponent,
-        RegistrarExtraordinariosFinanzasComponent,
-        RegistrarEgresoComponent
-    ],
-    exports: [
-        MatCardModule,
-        MatIconModule,
-        MatSelectModule
-    ],
-    providers: []
+  declarations: [
+    EgresosFinanzasComponent,
+    IngresosExtraordinariosFinanzasComponent,
+    IngresosFinanzasComponent,
+    RegistroMensualidadComponent,
+    CrearPagoMensualidadComponent,
+    RegistrarExtraordinariosFinanzasComponent,
+    RegistrarEgresoComponent
+  ],
+  exports: [
+    MatCardModule,
+    MatIconModule,
+    MatSelectModule
+  ],
+  providers: [
+  ]
 })
 export class FinanzasModule {
 
-    constructor() {
+  constructor() {
 
-    }
+  }
 
 }
